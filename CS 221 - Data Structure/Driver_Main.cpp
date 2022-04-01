b0f87520d34976542638f82e502116297fd82bca
Tony Allen Hardiman Jr <allenr64y@ymail.com>
2022-04-01 00:44:54 -0500

    Driver file for the program.

---
+++ CS 221 - Data Structure/Driver_Main.cpp
@@ -0,0 +1,16 @@
+/***********************************************
+*	Main Functiomn for project programming 1.
+***********************************************/
+
+#include <iostream>
+#include <string>
+#include "Test_Unit.h"
+
+int main(void) {
+	Test_Unit testing;
+
+	//Primarily Testing Point
+	testing.AutoTestFillLinkedList();
+
+	return 0;
+}
