b4d829cf4a880418c218e297dc118a0a7acce363
Tony Allen Hardiman Jr <allenr64y@ymail.com>
2022-04-01 00:46:29 -0500

    New Item class.

--- CS 221 - Data Structure/Item.h
+++ CS 221 - Data Structure/Item.h
@@ -3,22 +3,22 @@
 *	File: Item.h
 *	Author: Tony A. Hardiman Jr.
 *	Purpose: Defining a structure.
-*	Assignment 2: Programming Assignment 2
+*	Assignment 2: Programming Assignment 2 - Updated for Programming Assignment 3
 *	Declaration: This program is entirely my own work.
 *
 ****************************************/
+#pragma once
 
-#ifndef ITEM_H
-#define ITEM_H
-
-#include <string>
-#include <iostream>
+#define BATTLE_ITEM 1
+#define TREASURE_ITEM 2
 
 struct Item
 {
 	char m_sItemName[64];
+	char m_sDesc[127];
 	int m_Itype;
 	double m_dWeight;
 	double m_dValue;
+	Item* m_pLeft;
+	Item* m_pRight;
 };
-#endif
