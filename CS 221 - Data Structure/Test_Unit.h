5d34f6632b9fc7a669003ae0348af5af691d847b
Tony Hardiman Jr <allenr64y@ymail.com>
2022-04-12 23:31:31 -0500

    Update Test_Unit.h

--- CS 221 - Data Structure/Test_Unit.h
+++ CS 221 - Data Structure/Test_Unit.h
@@ -9,14 +9,21 @@ using namespace std;
 
 class Test_Unit {
 public:
+	Test_Unit();
+	~Test_Unit();
 	void AutoTestFillLinkedList();
 private:
 	Item* testRoot;
+	int counting;
+	string characterName[9];
+	string ItemName[10];
+	string ItemDesc[10];
 	Character* AddCharacterStats(Character* character, char* name, int* CharTraits, int &m_Class, int &m_Alignment, int &m_HitPoints);
 	void AttributeTesting(Character* newCharacter, int& TotalGrade, int& currentGrade, int* CharTraits, int& m_Class, int& m_Alignment, int& m_HitPoints);
 	void TestCharacterLocation(CharacterList* characterList, int& TotalGrade, int& currentGrade, int* CharTraits, int& m_Class, int& m_Alignment, int& m_HitPoints);
 	void TestLocation(CharacterList* list, int elementLocation, int& TotalGrade, int& currentGrade, int correctLocation);
-	void ItemTesting(CharacterList* characterList,char* charName, int& TotalGrade, int& CurrentGrade);
+	void ItemTesting(CharacterList* characterList,char* charName, int& TotalGrade, int& currentGrade);
+	void DeleteItemTesting(CharacterList* characterList, char* characterName, char* name, int& TotalGrade, int& currentGrade);
 	Item* CreateNewItem();
 	bool isTreeBalanced(Item* root);
 	int height(Item* rt);
